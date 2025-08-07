43dff9aff02ff54f7144b0bb35c93d38e3b32cd4
Sam Selikoff <sam.selikoff@gmail.com>
2025-08-07 15:31:44 -0700

    remove cacheComponents flag

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  experimental: {
-    cacheComponents: true,
-  },
+  /* config options here */
 };
 
 export default nextConfig;
